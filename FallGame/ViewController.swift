7d7f99f9bfba1fa87ec0949a0036f921fd6a0c29
椎木亮成 <shinoki.r.0301@gmail.com>
2015-06-29 14:34:42 +0900

    Initial Commit

---
+++ FallGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FallGame
+//
+//  Created by 椎木亮成 on 2015/06/29.
+//  Copyright (c) 2015年 shinokiryosei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
